fc0fd33fffe8d8655cf8189088b7dd423a6dd67d
mariaesilva <73125564+mariaesilva@users.noreply.github.com>
2020-10-19 15:42:08 -0300

    Update Header.tsx

--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 interface HeaderProps{
   title: string;
+  desc:string;
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
   return(
     <header>
       <h1>{props.title}</h1>
+      <h2>{props.desc}</h2>
     </header>
   )
 }
